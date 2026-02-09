e60e035ea30763f678ae10d738d149c4e36ee27d
Howlrr <91945857+Howlrr@users.noreply.github.com>
2026-02-09 14:51:13 +0300

    Update BotLogic.java

--- BotLogic.java
+++ BotLogic.java
@@ -1,18 +1,22 @@
 package TBot;
 
+
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class BotLogic {
     public static void main(String[] args) {
+        MarsStorage myStorage = new MarsStorage();
+        Main.fileManager manager = new Main.fileManager();
+        manager.loadThis(myStorage);
+
         try {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-          
-            botsApi.registerBot(new MyMarsBot());
 
-            System.out.println("🤖 Бот запущен! Можно писать ему в Телеграм.");
+            botsApi.registerBot(new MyMarsBot(myStorage));
+
+            System.out.println("🤖 Бот-завсклад запущен!");
         } catch (Exception e) {
-            System.out.println("❌ Ошибка при запуске: " + e.getMessage());
             e.printStackTrace();
         }
     }
